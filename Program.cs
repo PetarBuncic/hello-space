1d6af8aa5a248533668a6ed91d36d9344a74dadc
PetarBuncic <44311324+PetarBuncic@users.noreply.github.com>
2018-11-12 22:45:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace c_sharp_VSD_Projects
+{
+    class Program
+    
+    {
+        static public int n;
+       public static void Main(String [] args){
+            for (int i=0;i<100;i++){
+                 int n=i%3;
+                 int m=i%5;
+                 if(n==0){
+                     if(m==0){
+                        Console.WriteLine(i +" " + n +" " + m +" Deljiiv sa 3 i 5  ThreeFive" ); 
+                     }
+                     Console.WriteLine(i +" " + n +" " + m+" Deljiiv sa 3 THree"); 
+                 }
+                   else  if(m==0){
+                     Console.WriteLine(i +" " + n +" " + m+" Deljiiv sa 5 FIVE"); 
+                    }
+                 else{
+                     Console.WriteLine(i +" " + n +" " + m+" NIje Deljiiv NImand"); 
+                 }
+                 
+            }
+
+        }   
+    }
+}
